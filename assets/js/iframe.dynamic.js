2e079983fe50e0af104ced12dd6ea38b0b720a72
cleotayzq <cleotayzq@gmail.com>
2024-02-16 17:23:12 -0500

    Multi-page Navigation

--- assets/js/iframe.dynamic.js
+++
@@ -1,7 +0,0 @@
-/ Resize iframe to full height
-  function resizeIframe(height)
-  {
-    // "+60" is a general rule of thumb to allow for differences in
-    // IE & and FF height reporting, can be adjusted as required..
-    document.getElementById('shinyiframe').height = parseInt(height)+60;
-  }
