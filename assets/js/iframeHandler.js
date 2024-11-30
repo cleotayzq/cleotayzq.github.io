8639213bbbcfabcb1472a70b6e0f9f0c2fa4fdf8
cleotayzq <cleotayzq@gmail.com>
2024-11-29 17:46:16 -0800

    more finicky things

--- assets/js/iframeHandler.js
+++ assets/js/iframeHandler.js
@@ -1,17 +1,16 @@
-// shinyheight.js
+// iframeHandler.js
 
 // Listen for messages from the iframe
 window.addEventListener("message", function(event) {
-    console.log("Received message from iframe:", event.data); // Log the message
-
+    console.log("Received height from iframe:", event.data); // Debugging: log the received height
     var iframe = document.getElementById('shiny-frame');
 
-    // Optional: Validate the event origin for security
+    // Optional: Validate the origin for security (update with your Shiny app's URL)
     if (event.origin !== "https://cleotay.shinyapps.io") {
         console.warn("Unexpected origin:", event.origin);
         return;
     }
 
-    // Adjust iframe height based on the received data
+    // Adjust the iframe height to the received height
     iframe.style.height = event.data + 'px';
 });
